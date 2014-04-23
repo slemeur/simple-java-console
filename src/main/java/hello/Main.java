7e3524d23e30f8bcd781124ea87016980940e43a
Stevan Le Meur <stevan.lemeur@gmail.com>
2014-04-23 17:40:49 +0200

    Initial commit

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Main
+{
+    public static void main(String[] args) {
+       System.out.println(new  SayHello().sayHello(args[0]));
+       System.out.println("This is console app!");
+    }
+}
